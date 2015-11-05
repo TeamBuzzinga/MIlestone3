abc4d3074dc4b3f484bad117ef64d7d4f033b91a
Siddharth <siddharthshah52@gmail.com>
2015-11-04 20:40:11 -0500

    Some polish
    NPC runs after player on spotting the player
    NPC punches once pauses and punches again and this repeats until player
    is no longer very close to NPC
    Player executes takes damage animation (buggy)

---
+++ Milestone3/Assets/AI/Actions/catch_player2.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Action;
+using RAIN.Core;
+
+[RAINAction]
+public class catch_player2 : RAINAction
+{
+	private GameObject gameObject;
+	private Animator animator;
+
+	public override void Start(RAIN.Core.AI ai)
+	{
+		base.Start(ai);
+		gameObject = ai.WorkingMemory.GetItem<GameObject> ("varCharClose");
+		animator = gameObject.GetComponent<Animator> ();
+	}
+	
+	public override ActionResult Execute(RAIN.Core.AI ai)
+	{
+		animator.SetBool ("TakeDamage", true);
+		if(ai.WorkingMemory.ItemExists("catch_time"))
+		{
+			
+			int caught_time=ai.WorkingMemory.GetItem<int>("catch_time");
+			
+			if (caught_time <= 3)
+			{
+				caught_time++;
+				ai.WorkingMemory.SetItem<int>("catch_time",caught_time);
+			}
+			
+		}
+		
+		return ActionResult.SUCCESS;
+	}
+	
+	public override void Stop(RAIN.Core.AI ai)
+	{
+		base.Stop(ai);
+	}
+}
