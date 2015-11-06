ae6ab3fe54ed74c0790c99b431cc5ec331410aa3
ryanAndersen05 <ryan.andersen05@gmail.com>
2015-11-06 14:57:25 -0500

    Updates to prediction behavior

--- Milestone3/Assets/AI/Actions/prediction.cs
+++ Milestone3/Assets/AI/Actions/prediction.cs
@@ -7,20 +7,34 @@ using RAIN.Core;
 [RAINAction]
 public class prediction : RAINAction
 {
-	public GameObject player;
+    public Transform player;
+    Rigidbody playerRigid;
+    Vector3 futureLocation = new Vector3();
+    float calculateTimer;
+
     public override void Start(RAIN.Core.AI ai)
     {
         base.Start(ai);
-		player=GameObject.FindGameObjectWithTag("player");
+		player = GameObject.FindGameObjectWithTag("player").transform;
+        playerRigid = player.GetComponent<Rigidbody>();
     }
 
     public override ActionResult Execute(RAIN.Core.AI ai)
     {
-
-
-		location=ai.WorkingMemory.GetItem<Vector3>("location");
-
+        updateFutureLocation(ai);
+        
         return ActionResult.SUCCESS;
+    }
+
+    void updateFutureLocation(RAIN.Core.AI ai)
+    {
+        //calculateTimer += ai.WorkingMemory.GetItem<float>("calculateTime");
+        Vector3 playerFwd = player.forward;
+        Vector3 playerVel = playerRigid.velocity;
+        futureLocation = player.position + playerFwd * playerVel.magnitude * 3;
+        Debug.Log(futureLocation);
+
+        ai.WorkingMemory.SetItem<Vector3>("futureLocation", futureLocation);
     }
 
     public override void Stop(RAIN.Core.AI ai)
