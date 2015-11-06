c194b568ff6bb1c9501acfe73f2b18f941423f2d
dingfs <dingfengshao@gmail.com>
2015-11-06 15:56:28 -0500

    predicted NPC

--- Milestone3/Assets/Dingfengprefab/GameOverManager.cs
+++ Milestone3/Assets/Dingfengprefab/GameOverManager.cs
@@ -10,10 +10,12 @@ namespace CompleteProject
 
 		public GameObject NPC1;
 		public GameObject NPC2;
+		public GameObject NPC3;
 
 
 		private AIRig aiRig1 = null;
 		private AIRig aiRig2 = null;
+		private AIRig aiRig3 = null;
 
 		float restartTimer;
 		float restartDelay=5f;
@@ -24,6 +26,8 @@ namespace CompleteProject
             anim = GetComponent <Animator> ();
 			aiRig1 = NPC1.GetComponentInChildren<AIRig> ();
 			aiRig2 = NPC2.GetComponentInChildren<AIRig> ();
+			aiRig3 = NPC3.GetComponentInChildren<AIRig> ();
+
         }
 
 
@@ -31,10 +35,11 @@ namespace CompleteProject
         {
 			int catch1=aiRig1.AI.WorkingMemory.GetItem<int>("catch_time");
 			int catch2=aiRig2.AI.WorkingMemory.GetItem<int>("catch_time");
-		
+			int catch3=aiRig3.AI.WorkingMemory.GetItem<int>("catch_time");
 
 
-			if (catch1 > 2||catch2>2) {
+
+			if (catch1 > 2||catch2>2||catch3>0) {
 				anim.SetTrigger ("GameOver");
 				restartTimer+=Time.deltaTime;
 				if(restartTimer>=restartDelay)
